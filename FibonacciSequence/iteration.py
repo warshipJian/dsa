17ac405dde5c6be8e46687688eda64105ecb8be4
warshipJian <J.GY.jian@gmail.com>
2021-06-29 00:22:06 +0800

    test

--- FibonacciSequence/iteration.py
+++ FibonacciSequence/iteration.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+'''
+0,1
+1,2
+2,3
+'''
+
 def fib(n):
-    f = 1
+    f = 0
     g = 1
-    for v in range(n-1):
-        a = f
-        b = g
-        f = b
-        g = a + b
-    return f
+    while n > 1:
+        g = g + f
+        f = g - f
+        n -= 1
+    return g
+
 
 # 计算43 一下子就出来了哦
-print(fib(43))
+print(fib(10))
